bc9c5e1dc19a38e0a9f54570a3515f3d7e6a3448
myuon <ioi.joi.koi.loi@gmail.com>
2017-05-19 23:59:38 +0900

    Untyped, de Bruijn index, call-by-value

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,5 +1,6 @@
 import Test.Tasty
 import AExpTest
+import UntypedTest
 import SimplyTest
 import MuTest
 import RecursiveTest
@@ -7,6 +8,7 @@ import RecursiveTest
 main :: IO ()
 main = defaultMain $ testGroup "Tests"
   [ testGroup "UnAExp" aexpTests
+  , testGroup "Untyped" untypedTests
 --  testGroup "Simply" simplyTests,
 --  testGroup "Mu" muTests,
 --  testGroup "Recursive" recursiveTests
