91025746db3dd8b4ea0592f84662423d1eaf65e2
myuon <ioi.joi.koi.loi@gmail.com>
2017-09-04 20:34:29 +0900

    let in

--- src/Typed/SimplyExt.hs
+++ src/Typed/SimplyExt.hs
@@ -4,6 +4,8 @@ module Typed.SimplyExt
   , pattern Tunit
   , pattern Tstar
   , pattern (:.)
+  , pattern Tas
+  , pattern Tlet
   , Term(SimplyExtTerm)
   ) where
 
@@ -20,6 +22,14 @@ pattern (:.) tx ty = (Tabs "*" Tunit ty) `Tapp` tx
 
 pattern Tas t typ = Node "as" [t,typ]
 
+pattern Tlet x t1 t2 = Node "let _ = _ in _" [Tval x,t1,t2]
+
+data TypeOfError
+  = ExpectedType StrTree
+  deriving Show
+
+instance Exception TypeOfError
+
 instance Calculus "simply-ext" StrTree StrTree (M.Map Var Binding) where
   newtype Term "simply-ext" StrTree = SimplyExtTerm StrTree deriving (Eq, Show)
 
@@ -31,11 +41,42 @@ instance Calculus "simply-ext" StrTree StrTree (M.Map Var Binding) where
     rec ctx = rec' ctx . SimplyExtTerm
 
     go ctx Tstar = return Tunit
+    go ctx (Tas t typ) = do
+      tt <- rec ctx t
+      if tt == typ
+        then return typ
+        else throwM $ ExpectedType typ
+    go ctx (Tlet x t1 t2) = do
+      t1T <- rec ctx t1
+      t2T <- rec (M.insert x (VarBind t1T) ctx) t2
+      return t2T
     go ctx t = typeofR (\ctx' (SimplyTerm t) -> rec' ctx' (SimplyExtTerm t)) ctx (SimplyTerm t)
 
   evalR rec' ctx (SimplyExtTerm t) = fmap SimplyExtTerm $ go ctx t where
     rec ctx = fmap (\(SimplyExtTerm t) -> t) . rec' ctx . SimplyExtTerm
 
+    go ctx (Tas v typ)
+      | isValue (SimplyExtTerm v) = return v
+      | otherwise = fmap (\t -> Tas t typ) $ rec ctx v
+    go ctx (Tlet x v t)
+      | isValue (SimplyExtTerm v) = return $ subst x v t
+      | otherwise = do
+        v' <- rec ctx v
+        return $ Tlet x v t
     go ctx t = fmap (\(SimplyTerm t) -> t) $ evalR (\ctx' (SimplyTerm t) -> fmap (\(SimplyExtTerm t) -> SimplyTerm t) $ rec' ctx' (SimplyExtTerm t)) ctx (SimplyTerm t)
+
+    subst :: Var -> StrTree -> StrTree -> StrTree
+    subst x v = go where
+      go (Tsucc t) = Tsucc (go t)
+      go (Tpred t) = Tpred (go t)
+      go (Tif b t1 t2) = Tif (go b) (go t1) (go t2)
+      go (Tvar y)
+        | x == y = v
+        | otherwise = Tvar y
+      go (Tabs y yt t)
+        | x == y = Tabs y yt t
+        | otherwise = Tabs y yt (go t)
+      go (Tapp t1 t2) = Tapp (go t1) (go t2)
+      go t = t
 
 
--- test/Typed/SimplyExtTest.hs
+++ test/Typed/SimplyExtTest.hs
@@ -10,10 +10,12 @@ test_typeof =
   [ testCase "|- λx:A. x : A -> A" $ rights [typeof M.empty (SimplyExtTerm $ Tabs "x" Tbase (Tvar "x"))] @?= [Tarr Tbase Tbase]
   , testCase "|- λf:A -> A. λx:A. f(f(x)) : (A -> A) -> A -> A" $ rights [typeof M.empty (SimplyExtTerm $ Tabs "f" (Tbase `Tarr` Tbase) (Tabs "x" Tbase (Tvar "f" `Tapp` (Tvar "f" `Tapp` Tvar "x"))))] @?= [(Tbase `Tarr` Tbase) `Tarr` (Tbase `Tarr` Tbase)]
   , testCase "|- * : unit" $ rights [typeof M.empty (SimplyExtTerm $ Tstar)] @?= [Tunit]
+  , testCase "|- let x=2 in succ x : nat" $ rights [typeof M.empty (SimplyExtTerm $ Tlet "x" (Tsucc (Tsucc Tzero)) (Tsucc (Tvar "x")))] @?= [Tnat]
   ]
 
 test_eval =
   [ testCase "(λf:unit -> unit. f *) (λx:unit. x) -> *" $ rights [eval M.empty (SimplyExtTerm $ Tabs "f" (Tunit `Tarr` Tunit) (Tvar "f" `Tapp` Tstar) `Tapp` (Tabs "x" Tunit (Tvar "x")))] @?= [SimplyExtTerm Tstar]
+  , testCase "let x=2 in succ x -> 3" $ rights [eval M.empty (SimplyExtTerm $ Tlet "x" (Tsucc (Tsucc Tzero)) (Tsucc (Tvar "x")))] @?= [SimplyExtTerm (Tsucc (Tsucc (Tsucc Tzero)))]
   ]
 
 
