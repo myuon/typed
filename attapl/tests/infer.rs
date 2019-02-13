6bfb63aeec64a265b3a3ba2802a8ccd83086cada
myuon <ioi.joi.koi.loi@gmail.com>
2019-02-14 00:44:44 +0900

    test: for infer algorithm

---
+++ attapl/tests/infer.rs
@@ -0,0 +1,56 @@
+extern crate attapl;
+
+use attapl::*;
+
+#[test]
+fn check_infer_lambda() {
+    assert_eq!(
+        infer(
+            &mut Context::new(),
+            &Term::LambdaT(
+                "x".to_string(),
+                Box::new(Type::Bool),
+                Box::new(Term::VariableT("x".to_string())),
+            )
+        ),
+        Ok(
+            Type::Function(
+                Box::new(Type::Bool),
+                Box::new(Type::Bool),
+            )
+        ),
+    );
+}
+
+#[test]
+fn check_infer_context() {
+    assert_eq!(
+        infer(
+            &mut Context::from_vec(vec![
+                (
+                    "f".to_string(),
+                    Type::Function(
+                        Box::new(Type::Function(
+                            Box::new(Type::Bool),
+                            Box::new(Type::Bool),
+                        )),
+                        Box::new(Type::Bool),
+                    ),
+                )
+            ]),
+            &Term::ApplicationT(
+                Box::new(Term::VariableT("f".to_string())),
+                Box::new(
+                    Term::LambdaT(
+                        "x".to_string(),
+                        Box::new(Type::Bool),
+                        Box::new(Term::VariableT("x".to_string())),
+                    )
+                ),
+            )
+        ),
+        Ok(
+            Type::Bool
+        ),
+    );
+}
